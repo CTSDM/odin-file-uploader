55d7792a7f5c4637f8d9b97b560885b7af8116a2
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-11-16 17:35:25 +0900

    Update prisma schema to add url of remote storage server for files

---
+++ prisma/migrations/20241113142332_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `nameStorage` on the `files` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "files" DROP COLUMN "nameStorage";
