73ce3843aecad0198dcbb4e43b5bd96bb3d814d6
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-11-19 10:02:37 +0900

    Change action to upload files from /home to /file/upload

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -5,6 +5,5 @@ import directoryControllers from "../controllers/directory_controllers.js";
 const router = Router();
 
 router.get("/", directoryControllers.getHomepage);
-router.post("/", filesControllers.uploadFile);
 
 export default router;
