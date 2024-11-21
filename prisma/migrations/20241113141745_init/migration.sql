2fe3aaa21402167ee876fdf99442db92aad84e06
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-11-21 21:57:19 +0900

    Modify gitignore to ignore prisma migrations

--- prisma/migrations/20241113141745_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `urlStorage` to the `files` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "files" ADD COLUMN     "urlStorage" TEXT NOT NULL;
--- prisma/migrations/20241118050433_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name,extension,directoryId]` on the table `files` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "files_name_extension_directoryId_key" ON "files"("name", "extension", "directoryId");
